071c60e4ebb2b3ff10464a604858f3a82fae1a2e
Dawid Studzinski <dawid@compsolutions.pl>
2018-12-11 10:04:58 +0100

    craco issue 52
    npm run test
    press a (sometimes multiple times)

---
+++ craco.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  babel: {
+    presets: [],
+    plugins: [],
+    loaderOptions: { /* Any babel-loader configuration options: https://github.com/babel/babel-loader. */ },
+    // loaderOptions: (babelLoaderOptions, { env, paths }) => { return babelLoaderOptions; }
+  },
+  jest: {
+    babel: {
+      addPresets: true, // (default value)
+      addPlugins: true  // (default value)
+    },
+    configure: { /* Any Jest configuration options: https://jestjs.io/docs/en/configuration. */ },
+    // configure: (jestConfig, { env, paths, resolve, rootDir }) => { return jestConfig; }
+  },
+};
